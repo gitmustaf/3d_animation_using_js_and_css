0b17e16a5473865536ba41e5668c8b00dbbf52c1
Mustaf Hussain <mustafiaaa@gmail.com>
2021-01-24 20:31:37 +0530

    Added 3d effect to title and sneaker

--- main.js
+++ main.js
@@ -1,18 +1,28 @@
 //Animation logic
 const card = document.querySelector('.card');
 const container = document.querySelector('.container');
-
+const title = document.querySelector('.title');
+const sneaker = document.querySelector('.sneaker img');
 
 //Animation event
 container.addEventListener("mousemove", (e) => {
-    let xAxis = (window.innerWidth / 2 - e.pageX) / 25;
-    let yAxis = (window.innerHeight / 2 - e.pageY) / 25;
+    let xAxis = (window.innerWidth / 2 - e.pageX) / 15;
+    let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
     card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`
-    console.log(xAxis, yAxis);
+    // console.log(xAxis, yAxis);
 });
 
 //Reset card 
 container.addEventListener("mouseleave", (e) => {
+    card.style.transition = "all 0.5s ease";
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+    title.style.transform = "translateZ(0px)";
+    sneaker.style.transform = "translateZ(0px) rotate(0deg)";
 });
 
+//Animate In
+container.addEventListener("mouseinter", (e) => {
+    card.style.transition = "none";
+    title.style.transform = "translateZ(150px)";
+    sneaker.style.transform = "translateZ(200px) rotate(-45deg)";
+});
