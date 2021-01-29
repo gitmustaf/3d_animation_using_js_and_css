139bc57e36c55c82df901e4125394f7dde98e026
Mustaf Hussain <mustafiaaa@gmail.com>
2021-01-29 20:20:41 +0530

    3d effect is added to all the components

--- main.js
+++ main.js
@@ -3,6 +3,9 @@ const card = document.querySelector('.card');
 const container = document.querySelector('.container');
 const title = document.querySelector('.title');
 const sneaker = document.querySelector('.sneaker img');
+const description = document.querySelector('.info h3');
+const sizes = document.querySelector('.sizes');
+const purchase = document.querySelector('.purchase');
 
 //Animation event
 container.addEventListener("mousemove", (e) => {
@@ -18,11 +21,19 @@ container.addEventListener("mouseleave", (e) => {
     card.style.transform = `rotateY(0deg) rotateX(0deg)`;
     title.style.transform = "translateZ(0px)";
     sneaker.style.transform = "translateZ(0px) rotate(0deg)";
+    description.style.transform = "translateZ(0px)";
+    sizes.style.transform = "translateZ(0px)";
+    purchase.style.transform = "translateZ(0px)";
+    console.log("mouseleave");
 });
 
 //Animate In
-container.addEventListener("mouseinter", (e) => {
+container.addEventListener("mouseenter", (e) => {
     card.style.transition = "none";
-    title.style.transform = "translateZ(150px)";
-    sneaker.style.transform = "translateZ(200px) rotate(-45deg)";
+    title.style.transform = "translateZ(120px)";
+    sneaker.style.transform = "translateZ(120px) rotate(-45deg)";
+    description.style.transform = "translateZ(100px)";
+    sizes.style.transform = "translateZ(100px)";
+    purchase.style.transform = "translateZ(100px)";
+    console.log("mouse inrter");
 });
